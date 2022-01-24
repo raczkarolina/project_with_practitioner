f312a833f1d96225ba8b1e028b7010d25e947ede
Karolina Rączka <karolina.raczka@miquido.com>
2022-01-24 22:02:39 +0100

    added forecast for 7 days

--- main.py
+++ main.py
@@ -1,90 +1,95 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import folium
-import imageio
-from tqdm import tqdm_notebook
-from folium.plugins import MarkerCluster
-import geopandas as gpd
-import imageio
-import mapclassify as mc
+from statsmodels.tsa.stattools import adfuller
+from pmdarima import auto_arima
 import statsmodels.api as sm
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.arima_model import ARIMA
-import scipy
-from itertools import product
-import seaborn as sns
-from statsmodels.graphics.tsaplots import plot_pacf
-from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.tsa.arima_process import ArmaProcess
-from statsmodels.stats.diagnostic import acorr_ljungbox
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.stattools import pacf
-from statsmodels.tsa.stattools import acf
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import warnings
 
-plt.style.use('ggplot')
-plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.serif'] = 'Ubuntu'
-plt.rcParams['font.monospace'] = 'Ubuntu Mono'
-plt.rcParams['font.size'] = 14
-plt.rcParams['axes.labelsize'] = 12
-plt.rcParams['axes.labelweight'] = 'bold'
-plt.rcParams['axes.titlesize'] = 12
-plt.rcParams['xtick.labelsize'] = 12
-plt.rcParams['ytick.labelsize'] = 12
-plt.rcParams['legend.fontsize'] = 12
-plt.rcParams['figure.titlesize'] = 12
-plt.rcParams['image.cmap'] = 'jet'
-plt.rcParams['image.interpolation'] = 'none'
-plt.rcParams['figure.figsize'] = (12, 10)
-plt.rcParams['axes.grid']=True
-plt.rcParams['lines.linewidth'] = 2
-plt.rcParams['lines.markersize'] = 8
-colors = ['xkcd:pale orange', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta', 'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue',
-'xkcd:scarlet']
 
-data = pd.read_csv("dane_out_2016_2020.csv")
+data = pd.read_csv("dane_out_2016_2020.csv", parse_dates=True)
+# print(data.head())
 
 explodes = (0, 0.3)
 size = data.PM10_Al_Krasińskiego.isna().value_counts()
 
-PM10_Al_Krasińskiego_data = data[['data', 'godzina', 'temperatura_powietrza', 'PM10_Al_Krasińskiego']]
-# print(PM10_Al_Krasińskiego_data[PM10_Al_Krasińskiego_data.isna().any(axis=1)])
+PM10_Al_Krasinskiego_data = data[['data', 'godzina', 'PM10_Al_Krasińskiego']]
+# print(PM10_Al_Krasinskiego_data[PM10_Al_Krasinskiego_data.isna().any(axis=1)])
 
 
-PM10_Al_Krasińskiego_data.temperatura_powietrza=PM10_Al_Krasińskiego_data.temperatura_powietrza.fillna(method='bfill')
-PM10_Al_Krasińskiego_data.PM10_Al_Krasińskiego=PM10_Al_Krasińskiego_data.PM10_Al_Krasińskiego.fillna(method='bfill')
+# PM10_Al_Krasinskiego_data.temperatura_powietrza = PM10_Al_Krasinskiego_data.temperatura_powietrza.fillna(method='bfill')
+PM10_Al_Krasinskiego_data.PM10_Al_Krasińskiego = PM10_Al_Krasinskiego_data.PM10_Al_Krasińskiego.fillna(method='bfill')
 
-PM10_Al_Krasińskiego_data.godzina=PM10_Al_Krasińskiego_data.godzina.fillna('0:00:00')
-PM10_Al_Krasińskiego_data.data=PM10_Al_Krasińskiego_data.data.fillna(method='ffill')
+PM10_Al_Krasinskiego_data.godzina = PM10_Al_Krasinskiego_data.godzina.fillna('0:00:00')
+PM10_Al_Krasinskiego_data.data = PM10_Al_Krasinskiego_data.data.fillna(method='ffill')
 
-# print(PM10_Al_Krasińskiego_data.head())
+PM10_Al_Krasinskiego_data.set_index(['data', 'godzina'], inplace=True, append=False, drop=True)
 
-PM10_Al_Krasińskiego_data['data'] = pd.to_datetime(PM10_Al_Krasińskiego_data.data)
-# print(PM10_Al_Krasińskiego_data['data'])
+print(PM10_Al_Krasinskiego_data.head())
 
-YEAR = []
-MONTH = []
-DAY = []
-WEEKDAY = []
-for i in range(len(PM10_Al_Krasińskiego_data)):
-    WEEKDAY.append(PM10_Al_Krasińskiego_data.data[i].weekday())
-    DAY.append(PM10_Al_Krasińskiego_data.data[i].day)
-    MONTH.append(PM10_Al_Krasińskiego_data.data[i].month)
-    YEAR.append(PM10_Al_Krasińskiego_data.data[i].year)
+PM10_Al_Krasinskiego_data['PM10_Al_Krasińskiego'].plot(figsize=(12, 5))
+plt.show()
 
-PM10_Al_Krasińskiego_data['Year'] = YEAR
-PM10_Al_Krasińskiego_data['Month'] = MONTH
-PM10_Al_Krasińskiego_data['Day'] = DAY
-PM10_Al_Krasińskiego_data['Weekday'] = WEEKDAY
 
-change_year_index = []
-change_year = []
-year_list = PM10_Al_Krasińskiego_data['Year'].tolist()
-for y in range(0,len(year_list)-1):
-    if year_list[y]!=year_list[y+1]:
-        change_year.append(year_list[y+1])
-        change_year_index.append(y+1)
+def ad_test(dataset):
+    dftest = adfuller(dataset, autolag='AIC')
+    print("1. ADF : ", dftest[0])
+    print("2. P-Value : ", dftest[1])
+    print("3. Num Of Lags : ", dftest[2])
+    print("4. Num Of Observations Used For ADF Regression:", dftest[3])
+    print("5. Critical Values :")
+    for key, val in dftest[4].items():
+        print("\t", key, ": ", val)
 
-print(PM10_Al_Krasińskiego_data.loc[change_year_index].head())
+
+ad_test(PM10_Al_Krasinskiego_data['PM10_Al_Krasińskiego'])
+
+# p> 0,05; Dane nie są stacjonarne
+# Nie jest to sztywna reguła, ale dane stacjonarne powinny mieć małą wartość p. Większa wartość p może wskazywać na występowanie pewnych trendów (różna średnia) lub sezonowość.
+
+stepwise_fit = auto_arima(PM10_Al_Krasinskiego_data['PM10_Al_Krasińskiego'], trace=True, suppress_warnings=True)
+
+print(PM10_Al_Krasinskiego_data.shape)
+train = PM10_Al_Krasinskiego_data.iloc[:-24]
+test = PM10_Al_Krasinskiego_data.iloc[-24:]
+print(train.shape, test.shape)
+
+warnings.filterwarnings("ignore")
+model = sm.tsa.arima.ARIMA(train['PM10_Al_Krasińskiego'], order=(1, 1, 1))
+model = model.fit()
+model.summary()
+
+
+start = len(train)
+end = len(train)+len(test)-1
+pred = model.predict(start=start, end=end, typ='levels').rename('ARIMA Predictions')
+pred.index = PM10_Al_Krasinskiego_data.index[start:end+1]
+pred.plot(figsize=(12,10), legend=True)
+test['PM10_Al_Krasińskiego'].plot(legend=True)
+plt.setp(plt.xticks()[1], rotation=30, ha='right')
+plt.show()
+
+test['PM10_Al_Krasińskiego'].mean()
+rmse = sqrt(mean_squared_error(pred, test['PM10_Al_Krasińskiego']))
+print(rmse)
+
+
+#PREDICTION
+model2 = sm.tsa.arima.ARIMA(PM10_Al_Krasinskiego_data['PM10_Al_Krasińskiego'], order=(1, 1, 1))
+model2 = model2.fit()
+PM10_Al_Krasinskiego_data.tail()
+
+dt_pred = pd.read_csv("dane/gios-pjp-data.csv", parse_dates=True)
+dt_pred = dt_pred[['PM10_Al_Krasińskiego']]
+print(dt_pred.head())
+
+
+index_future_dates = pd.date_range(start='2021-10-30', end='2021-11-06', freq='60min')
+pred = model2.predict(start=len(PM10_Al_Krasinskiego_data), end=len(PM10_Al_Krasinskiego_data)+168, typ='levels').rename('ARIMA Predictions')
+pred.index = index_future_dates
+pred.plot(figsize=(12, 5), legend=True)
+dt_pred['PM10_Al_Krasińskiego'].plot(legend=True)
+plt.setp(plt.xticks()[1], rotation=30, ha='right')
+plt.show()
+
